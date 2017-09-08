71d195bbe412287e0fd73c3393cf5ceb6fca6dbd
Nienke Nobel <nnobel@sogyo.nl>
2017-09-08 16:52:57 +0200

    Training no longer required

--- ModelBase/ModelBase_MNIST.py
+++ ModelBase/ModelBase_MNIST.py
@@ -7,6 +7,9 @@ import Testing as test
 Loosely inspired by http://abel.ee.ucla.edu/cvxopt/_downloads/mnist.py
 which is GPL licensed.
 """
+toTrain = input("Pretrained model? (y/n): ") == "n"
+
+print("Initializing...")
 
 training_data = list(
     dataPrep.read(dataset='training', path='C:/Users/nnobel/Vrijdagmiddagproject_Jesper_Myrthe_Nienke/Datasets'))
@@ -14,6 +17,7 @@ training_data = list(
 testing_data = list(
     dataPrep.read(dataset='testing', path='C:/Users/nnobel/Vrijdagmiddagproject_Jesper_Myrthe_Nienke/Datasets'))
 
+print("Reshaping Data...")
 training_answers, training_images = dataPrep.returnDataToUse(training_data)
 
 testing_answers, testing_images = dataPrep.returnDataToUse(testing_data)
@@ -31,12 +35,12 @@ which is GPL licensed.
 """
 
 """------------------------------TRAINING------------------------------------"""
-clf = svm.SVC(C=0.001, kernel='linear')
-
-print("Training...")
-train.training(training_images, training_answers, 'lerenLezen.pkl', clf)
+if toTrain :
+    print("Training...")
+    clf = svm.SVC(C=0.001, kernel='linear')
+    train.training(training_images, training_answers, 'latestNN.pkl', clf)
 
 """------------------------------Testing Function----------------------------"""
 print("Testing...")
-test.testing(testing_images, testing_answers, 'lerenLezen.pkl')
+test.testing(testing_images, testing_answers, 'latestNN.pkl')
 
